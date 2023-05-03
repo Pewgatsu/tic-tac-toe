08d929b91725e0817eb6842347d7e16142803d72
james <jamesdeleon373@gmail.com>
2023-05-03 18:17:00 +0800

    Added win condition for rows and columns

--- index.js
+++ index.js
@@ -38,31 +38,81 @@ const displayController = (() => {
 
   boardCell.forEach((cell) => {
     const cellIndex = cell.getAttribute("data-index");
-    cell.addEventListener("click", (e) => {
-      e.target.textContent = gameController.getPlayerSign();
+    cell.addEventListener("click", () => {
       gameController.playerMove(cellIndex);
+      updateGameBoard();
     });
   });
+
+  const updateGameBoard = () => {
+    for (let i = 0; i < boardCell.length; i++) {
+      boardCell[i].textContent = gameBoard.getCell(i);
+    }
+  };
+
+  return { updateGameBoard };
 })();
 
 const gameController = (() => {
   const playerX = Player("X");
   const playerO = Player("O");
   let round = 1;
+  let gameOver = false;
 
   const playerMove = (index) => {
-    if (gameBoard.getCell(index) === undefined) {
+    const boardCell = gameBoard.getCell(index);
+    if (boardCell === undefined) {
       gameBoard.setCell(index, getPlayerSign());
-    } else {
-      console.log("invalid");
+      round += 1;
     }
-
-    console.log(round);
-    round +=1;
+    if (round === 9) {
+      gameOver = true;
+    }
   };
+
+  const checkRows = () => {
+
+    for (let row = 0; row < 3; row++) {
+      const rows = [];
+      for (let col = row * 3; col < row * 3 + 3; col++) {
+        rows.push(gameBoard.getCell(col));
+      }
+
+      if (rows.every((cell) => cell === "X") || rows.every((cell) => cell === "O")){
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const checkColumns = () => {
+
+    for (let col = 0; col < 3; col++) {
+      const columns = [];
+      for (let row = 0; row < 3; row++ ){
+        columns.push(gameBoard.getCell(col + row * 3));
+
+        if (columns.every((cell) => cell === "X") || columns.every((cell) => cell === "O")){
+          return true;
+        }
+      }
+      
+      
+    }
+    return false;
+  }
 
   const getPlayerSign = () =>
     round % 2 === 1 ? playerX.getSign() : playerO.getSign();
 
-  return { playerMove, getPlayerSign };
+  const getGameStatus = () => gameOver;
+
+  const checkWinner = () => {
+    if (getGameStatus() || checkRows() || checkColumns()) {
+      return true;
+    }
+    return false;
+  };
+
+  return { playerMove, getPlayerSign, getGameStatus, checkWinner };
 })();
